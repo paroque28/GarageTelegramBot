ff61c5ff702f2bdb75b2c05195790c8fc0db9b7a
prodrigu <paroque28@gmail.com>
2019-01-01 18:00:33 -0600

    import threads maybe

--- telegram-frontend/src/main.py
+++ telegram-frontend/src/main.py
@@ -24,6 +24,7 @@ import os
 import logging
 import constants as c
 from pathlib import Path
+from threading import Thread
 VERSION = 1.0
 home = str(Path.home())
 # Enable logging
@@ -200,10 +201,10 @@ def gates_sensor_handler(gpio):
         subscribers = db.get_subscribers(gate)
         if (new_state == c.OPEN_GPIO):
             send_to_subscribers(bot, subscribers, "Mensaje de subscripcion:\n Porton "+ str(gate) + " fue abierto")
-            if (c.DEBUG>1):
+            if (c.DEBUG>2):
                 print("start thread")
             Thread(target = timer_close_gate, args = (bot, gate,)).start()
-            if (c.DEBUG>1):
+            if (c.DEBUG>2):
                 print("started thread")
         elif (new_state == c.CLOSED_GPIO):
             send_to_subscribers(bot, subscribers, "Mensaje de subscripcion:\n Porton "+ str(gate) + " fue cerrado")
